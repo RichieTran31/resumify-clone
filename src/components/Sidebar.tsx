c19673e4e98d498c45628998866d334e96c9f3c5
RichieTran31 <richietran11@gmail.com>
2025-10-28 16:00:48 -0400

    Update Sidebar.tsx

--- src/components/Sidebar.tsx
+++ src/components/Sidebar.tsx
@@ -10,7 +10,7 @@ export default function Sidebar() {
       <div className="mb-6">
         <div className="w-32 h-32 rounded-full border-4 border-white overflow-hidden">
           <Image
-            src="https://media.licdn.com/dms/image/v2/D5603AQGZO6T5x3tsug/profile-displayphoto-shrink_400_400/profile-displayphoto-shrink_400_400/0/1694300338754?e=1762992000&v=beta&t=RSDlbYCScO_Qop5U4j99vWh5N-9YaLri_1wCHnVPlfw"
+            src="\_Richie Tran.jpg"
             alt="Richie Tran"
             width={128}
             height={128}
@@ -22,7 +22,7 @@ export default function Sidebar() {
       {/* Name and Title */}
       <div className="text-center mb-8">
         <h1 className="text-3xl font-semibold mb-2">Richie Tran</h1>
-        <p className="text-lg text-white/90">Data Analyst</p>
+        <p className="text-lg text-white/90">Data Analyst | First Generation Graduate | </p>
       </div>
 
       {/* Social Links */}
@@ -36,19 +36,11 @@ export default function Sidebar() {
             className="w-6 h-6"
           />
         </Link>
-        <Link href="https://www.facebook.com/" className="hover:scale-110 transition-transform">
-          <Image
-            src="https://ext.same-assets.com/627721302/2563901416.svg"
-            alt="Facebook"
-            width={24}
-            height={24}
-            className="w-6 h-6"
-          />
-        </Link>
       </div>
+
       {/* Copyright */}
       <div className="absolute bottom-8 text-sm text-white/80">
-        © 2025 All rights reserved.
+        © 2023 All rights reserved.
       </div>
     </div>
   );
